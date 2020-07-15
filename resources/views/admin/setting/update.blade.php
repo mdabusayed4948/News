ff3763f78a70ebe727dfb74050ed19a9228e401d
sayedme120 <sayedme120@gmail.com>
2020-07-16 04:38:44 +0600

    add image tag and others task

---
+++ resources/views/admin/setting/update.blade.php
@@ -0,0 +1,104 @@
+@extends('admin.layout.master')
+@section('title','Update Setting')
+@push('css') {{-- Create CSS --}}
+
+@endpush
+
+@section('content')
+
+
+
+    <div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>{{ $page_name }}</h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8">
+            <div class="page-header float-right">
+                <div class="page-title">
+                    <ol class="breadcrumb text-right">
+                        <li><a href="{{ url('back/') }}">Dashboard</a></li>
+                        <li class="active">{{ $page_name }}</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">{{ $page_name }}</strong>
+                        </div>
+                        <div class="card-body">
+                            <!-- Credit Card -->
+                            <div id="pay-invoice">
+                                <div class="card-body">
+
+                                    @if(count($errors) > 0)
+                                        <div class="alert  alert-danger alert-dismissible fade show" role="alert">
+
+                                            <span class="badge badge-pill badge-danger">Errors</span>
+
+
+                                            @foreach($errors->all() as $i =>$error)
+
+                                                <span class="badge badge-pill badge-warning">{{ ++$i}}.</span> {{ $error }}
+                                            @endforeach
+
+
+
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                    @endif
+
+
+                                    {{ Form::open(['url' => '/back/setting/update','method'=>'put','enctype'=>'multipart/form-data']) }}
+
+                                    <div class="form-group">
+
+                                        {{ Form::label('name', 'System Name', array('class'=>'control-label mb-1')) }}
+
+                                        {{ Form::text('name', $system_name, ['class'=>'form-control', 'id'=>'name']) }}
+
+                                    </div>
+
+                                    <div>
+                                        <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
+                                            <i class="fa fa-save fa-lg"></i>&nbsp;
+                                            <span id="payment-button-amount">Submit</span>
+                                            <span id="payment-button-sending" style="display:none;">Sending…</span>
+                                        </button>
+                                    </div>
+                                    {{-- </form> --}}
+
+                                    {{ Form::close() }}
+                                </div>
+                            </div>
+
+                        </div>
+                    </div> <!-- .card -->
+
+                </div>
+
+
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+
+
+
+@endsection
+
+@push('js') {{-- Create JS --}}
+
+@endpush
